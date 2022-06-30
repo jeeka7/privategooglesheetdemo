3241b38e58332c152cbeabd25e5f79d3a33c9d29
Dean Hunter <jeeka7@gmail.com>
2022-07-01 02:38:46 +0530

    Update students.py

--- students.py
+++ students.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import gspread
 
+sa = gspread.service_account()
+sh = sa.open("tutionmanagement")
+
+wks = sh.worksheet("student")
+
 st.title(" Tution management ")
+
+st.write('Rows: ', wks.row_count)
+st.write('Cols: ', wks.col_count)
 
