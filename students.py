5bb1d59f329cbc6a983ac66f7960f48d89c541ff
Dean Hunter <jeeka7@gmail.com>
2022-07-04 05:27:02 +0530

    Update students.py

--- students.py
+++ students.py
@@ -5,12 +5,5 @@ import gspread
 
 
 service_account = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
-#sh = sa.open("tutionmanagement")
 
-#wks = sh.worksheet("student")
-
-#st.title(" Tution management ")
-
-#st.write('Rows: ', wks.row_count)
-#st.write('Cols: ', wks.col_count)
-
+st.write(st.secrets.keys())
