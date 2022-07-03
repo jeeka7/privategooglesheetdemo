ceeeff30e4ab2e794d2be260f49820d6b132fe4d
Dean Hunter <jeeka7@gmail.com>
2022-07-04 05:28:51 +0530

    Update students.py

--- students.py
+++ students.py
@@ -6,4 +6,4 @@ st.write(st.secrets)
 
 service_account = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
 
-st.write(st.secrets.keys())
+st.write(st.secrets["private_gsheets_url"])
