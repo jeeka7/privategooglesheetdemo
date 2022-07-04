888f09e8d7819f3c63c170f6c8ab0e2b6b4d3400
Dean Hunter <jeeka7@gmail.com>
2022-07-04 05:45:54 +0530

    Update students.py

--- students.py
+++ students.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import gspread
+import pandas as pd
 
 st.write(st.secrets)
 
@@ -8,4 +9,13 @@ service_account = gspread.service_account_from_dict(st.secrets["gcp_service_acco
 
 st.write(st.secrets["private_gsheets_url"])
 
-service_account.open_by_url(st.secrets["private_gsheets_url"])
+sht = service_account.open_by_url(st.secrets["private_gsheets_url"])
+
+
+worksheet = sht.worksheet(students)
+
+get_values = worksheet.get_all_values()
+
+df= pd.DataFrame(get_values)
+
+st.write(get_values)
