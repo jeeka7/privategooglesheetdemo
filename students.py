9fed63d9ccfae06e60ae9ef00ed80ea59c110c11
Dean Hunter <jeeka7@gmail.com>
2022-07-04 05:32:55 +0530

    Update students.py

--- students.py
+++ students.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import gspread
 
-st.write(st.secrets)
+#st.write(st.secrets)
 
 
 service_account = gspread.service_account_from_dict(st.secrets["gcp_service_account"])
 
-st.write(st.secrets["private_gsheets_url"])
+#st.write(st.secrets["private_gsheets_url"])
 
 service_account.open_by_url(st.secrets["private_gsheets_url"])
